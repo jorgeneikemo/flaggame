f4fd32a57ca24a42ad3278f51a134dab29827d75
jorgen eikemo <jorgen.eikemo@gmail.com>
2023-01-24 21:18:55 +0100

    added functionality and terrible styling

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let countryList=[{}];
-let questionText;
 let randomFlagImg = [];
 let randomFlagImgInfo = [];
 let randomNumberArray = [];
+
 let points = 0;
+let rounds = 1;
+const maxRounds = 10;
+
+let questionText;
 let pointsText;
+let statusText;
+let roundsText;
 
 window.onload = async function(){
     questionText = document.getElementById("question");
     pointsText =  document.getElementById("points");
     randomFlagImg = document.querySelectorAll(".flagimg");
-    for(i=0; i< randomFlagImg.length; i++){
-        randomFlagImg[i].addEventListener("click", checkAnswer);
-    }
+    statusText = document.getElementById("statustext");
+    roundsText = document.getElementById("rounds");
 
     getData();
 
@@ -28,35 +33,77 @@ async function getData(){
     setUpGame();
 }
 function setUpGame(){
+    //make the flags clickable
+    for(i=0; i< randomFlagImg.length; i++){
+        randomFlagImg[i].addEventListener("click", checkAnswer);
+        randomFlagImg[i].classList.remove("incorrect");
+        randomFlagImg[i].classList.remove("correct");
+    }
+
     //create 4 unique random numbers to choose info from the countryList.
     while(randomNumberArray.length < 4){
         var r = Math.floor(Math.random() * countryList.length);
         if(randomNumberArray.indexOf(r) === -1) randomNumberArray.push(r);
     }
     
-
+    //select random image and log the name.
     for(i=0; i<4; i++){
         randomFlagImg[i].src = countryList[randomNumberArray[i]].flag;
         randomFlagImgInfo[i] = countryList[randomNumberArray[i]].name;
-        console.log(randomNumberArray[i]);
-        console.log(randomFlagImgInfo[i]);
     }
 
     questionText.innerText = randomFlagImgInfo[Math.floor(Math.random() * 4)];
-    pointsText.innerText = "Points: " + points.toString();
+    statusText.innerText = "";
+    roundsText.innerText = "Round " + rounds + "/" + maxRounds;
+    
 
 }
 
 function checkAnswer(){
-    if(randomFlagImgInfo[this.id] === questionText.innerText){
-        alert("Correct!");
+    if(randomFlagImgInfo[this.id].toUpperCase() === questionText.innerText){
+        //alert("Correct!");
+        randomFlagImg[this.id].classList.add("correct");
         randomNumberArray = [];
+
+        //change the points and count down round
         points += 1;
-        setUpGame();
+        rounds += 1;
+
+        pointsText.innerText = "Points: " + points.toString();
+        statusText.innerText = "Correct!";
+        statusText.style.color = "green";
+
+        newRound();
     }
     else{
-        alert("Wrong!");
+        //clear the random number array.
+        randomFlagImg[this.id].classList.add("incorrect");
         randomNumberArray = [];
-        setUpGame();
+
+        rounds += 1;
+
+        statusText.innerText = "Wrong!";
+        statusText.style.color = "red";
+
+        newRound();
+    }
+}
+
+function newRound(){
+    //prevent the user from clicking again
+    removeEventListener();
+
+    if(rounds!=maxRounds){
+    //restart the game after timeout
+    setTimeout(setUpGame,2000);
+    }
+    else{
+        roundsText.innerText = "Game over!";
+    }
+}
+
+function removeEventListener(){
+    for(i=0; i< randomFlagImg.length; i++){
+        randomFlagImg[i].removeEventListener("click", checkAnswer);
     }
 }
